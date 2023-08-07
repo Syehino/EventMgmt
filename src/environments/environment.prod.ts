ebb08d791526d6e571b74cfdfc401d530ea8fee9
Syehino <syehino147@gmail.com>
2023-08-07 15:26:38 +0800

    fixed https issue for api in android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  apiEndpoint: 'https://syednazirul-organic-lamp-pjrjwrjgqp6f69p4-3000.app.github.dev/api/events',
+  production: true,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiEndpoint: 'https://20.205.156.61:3000/api/events',
+  apiEndpoint: 'https://syednazirul-organic-lamp-pjrjwrjgqp6f69p4-3000.app.github.dev/api/events',
 };
 
 /*
